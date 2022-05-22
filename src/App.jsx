69b64fd1dec137fd8b04b3c96891e33ff5e8705f
Yanis Kolli <yanis.kolli@ens.uvsq.fr>
2022-05-22 21:32:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import UserForm from './UserForm'
+import GameApp from './GameApp'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './firebase'
+
+export default function App() {
+    const [user, loading, error] = useAuthState(auth)
+
+    if (loading) {
+        return 'Now loading ...'
+    }
+    if (error) {
+        return 'Error, please try again'
+    }
+    if (!user) {
+        return <UserForm />
+    }
+
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/">
+                  <Route exact path='/' element={<Home/>}/>
+                </Route>
+                <Route path="/game/:id">
+                    <Route path="/game/:id" element={<GameApp/>}/>
+                </Route>
+            </Routes>
+        </Router>
+    )
+}
